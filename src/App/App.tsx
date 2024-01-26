2ac1263c5332f9ab262334666bcb13ccda1f1513
Akindeju Oluwagbemiga Michael <akindejuhmikel@gmail.com>
2024-01-26 21:35:18 +0100

    [SETUP]: setup tailwind-css, node-sass

---
+++ src/App/App.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import './App.scss';
+
+const App: React.FunctionComponent = () => {
+    return (
+        <div className="App">
+            <header className="App-header">
+                <p className="text-[50px] font-bold underline">
+                    Edit <code>src/App.tsx</code> and save to reload.
+                </p>
+            </header>
+        </div>
+    );
+};
+
+export default App;
